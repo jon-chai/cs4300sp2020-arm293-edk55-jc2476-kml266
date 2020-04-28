41215d55a7a845d52e48bac6bb5c5d7ebf01cb91
Kevin <kml266@cornell.edu>
2020-04-27 23:44:00 -0400

    rush

--- model.py
+++ model.py
@@ -3,12 +3,17 @@ import pickle
 import ast
 import pandas as pd
 from nltk.stem import WordNetLemmatizer
-
 lemmatizer = WordNetLemmatizer()
+from nltk.corpus import stopwords
+import gensim.downloader as api
+
 def process():
 
     recipes = {}
     inv_idx = {}
+    flavors = ['sweet', 'salty', 'savory', 'sour', 'spicy', 'bitter', 'citrus', 'fruity', 'rich']
+    stop_words = set(stopwords.words('english'))
+    wv = api.load('glove-twitter-25')
 
     with open('RAW_recipes.csv', encoding='utf-8') as f:
 
@@ -31,6 +36,7 @@ def process():
             recipes[row_id]['steps'] = ast.literal_eval(row['steps'])
             recipes[row_id]['nutrition'] = ast.literal_eval(row['nutrition'])
             recipes[row_id]['tags'] = ast.literal_eval(row['tags'])
+            recipes[row_id]['flavors'] = {}
 
             recipes[row_id]['features'] = set()
 
@@ -46,6 +52,8 @@ def process():
 
             for ingredient in recipes[row_id]['name'].split():
                 recipes[row_id]['features'].add(lemmatizer.lemmatize(ingredient))
+            
+            
 
             for ingredient in recipes[row_id]['tags']:
                 recipes[row_id]['features'].add(lemmatizer.lemmatize(ingredient))
@@ -54,7 +62,22 @@ def process():
                 if ingredient not in inv_idx:
                     inv_idx[ingredient] = set()
                 inv_idx[ingredient].add(row_id)
-
+                     
+                    
+            good_ingredients = [lemmatizer.lemmatize(ingredient) for ingredient in recipes[row_id]['name'].split() if ingredient not in stop_words]
+            
+            for flavor in flavors:
+                sim = 0
+                tot = .01
+                for ingredient in good_ingredients:
+                    try:
+                        sim += wv.similarity(ingredient, flavor)
+                        tot += 1
+                    except KeyError:
+                        continue
+                        
+                recipes[row_id]['flavors'][flavor] = round(sim / tot, 2)    
+                    
     return recipes, inv_idx
 
 class IngredientIndex:
@@ -64,9 +87,13 @@ class IngredientIndex:
 
         self.recipes = recipes
         self.inv_idx = inv_idx
+        self.flavors = set(item for item in ['sweet', 'salty', 'savory', 'sour', 'spicy', 'bitter', 'citrus', 'fruity', 'rich'])
 
     def get_id(self, item):
-        return self.inv_idx[lemmatizer.lemmatize(item)]
+        item = lemmatizer.lemmatize(item)
+        if item in self.inv_idx:
+            return self.inv_idx[lemmatizer.lemmatize(item)]
+        return set()
 
     def __getitem__(self, item):
         recipe_ids = self.get_id(item)
@@ -116,11 +143,28 @@ class IngredientIndex:
 
     # Returns set of recipes such that every ingredient in good_ingredient_list is included in each recipe
     # and every ingredient in bad_ingredient_list is not included in each recipe
-    def search(self, good_ingredient_list, bad_ingredient_list):
+    def search(self, good_ingredient_list, bad_ingredient_list, flavor_list):
 
-        return [(recipe_id,self.recipes[recipe_id]) for recipe_id in
-                self.intersect(good_ingredient_list).intersection(self.exclude(bad_ingredient_list))]
+#         good_recipes =  [(recipe_id,self.recipes[recipe_id]) for recipe_id in
+#                 self.intersect(good_ingredient_list).intersection(self.exclude(bad_ingredient_list))]
+        
+        good_recipes =  [(recipe_id, 0) for recipe_id in self.intersect(good_ingredient_list).intersection(self.exclude(bad_ingredient_list))]
+        new_recipes = []
+        flavor_list = [flavor for flavor in flavor_list if flavor in self.flavors]
+        print(flavor_list)
+        n_flavors = len(flavor_list)
+        
+        if n_flavors != 0:
+            for recipe_id, score in good_recipes:
+                for flavor in flavor_list:
+                    score += self.recipes[recipe_id]['flavors'][flavor]
+                new_recipes.append((recipe_id, score))
 
+            good_recipes = sorted(new_recipes, key=lambda x: x[1], reverse=True)
+            
+        return [(x[0], self.recipes[x[0]]) for x in good_recipes]
+           
+                    
 if __name__ == '__main__':
     recipes, inv_idx = process()
     with open('recipes.pkl', 'wb') as f:
